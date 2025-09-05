bdbfea823df0555c99ee7ab6e0c8c5741b94d6b2
iutgeiitoulonE105 <touloniut@gmail.com>
2025-09-05 12:29:07 +0200

    update robot

--- Embedded/Robot_Anthony_Castello.X/IO.h
+++ Embedded/Robot_Anthony_Castello.X/IO.h
@@ -7,6 +7,12 @@
 #define LED_ORANGE_1 _LATJ4 
 #define LED_ROUGE_1 _LATJ11
 #define LED_VERTE_1 _LATH10
+#define LED_ORANGE_2 _LATK15
+#define LED_VERTE_2 _LATH3
+#define LED_ROUGE_2 _LATA10
+#define LED_BLEUE_2 _LATA9
+#define LED_BLANCHE_2 _LATA0
+
 
 // Prototypes fonctions
 void InitIO();
--- Embedded/Robot_Anthony_Castello.X/main.c
+++ Embedded/Robot_Anthony_Castello.X/main.c
@@ -3,6 +3,7 @@
 #include <xc.h>
 #include "ChipConfig.h"
 #include "IO.h"
+#include "timer.h"
 
 int main(void) {
     /***********************************************************************************************/
@@ -13,15 +14,24 @@ int main(void) {
     // Configuration des input et output (IO)
     /***********************************************************************************************/
     InitIO();
-
+    InitTimer1();
+    InitTimer23();
+    _T1Interrupt();
+    _T3Interrupt();
     LED_BLANCHE_1 = 1;
     LED_BLEUE_1 = 1;
     LED_ORANGE_1 = 1;
     LED_ROUGE_1 = 1;
     LED_VERTE_1 = 1;
+    LED_BLANCHE_2 = 1;
+    LED_BLEUE_2 = 1;
+    LED_ORANGE_2 = 1;
+    LED_ROUGE_2 = 1;
+    LED_VERTE_2 = 1;
     /***********************************************************************************************/
     // Boucle Principale
     /***********************************************************************************************/
     while (1) {
-    } // fin main
+        
+   } // fin main
 }
