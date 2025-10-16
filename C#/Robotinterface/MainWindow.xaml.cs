954179e7a7903d254475d4c5e4b4086a17b7027f
iutgeiitoulonE105 <touloniut@gmail.com>
2025-10-16 12:23:30 +0200

    robotinterface (bouton qui change de couleur et qui clc)

--- C#/Robotinterface/MainWindow.xaml.cs
+++ C#/Robotinterface/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -9,16 +10,32 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RobotInterface
+namespace Robotinterface
 {
+    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool toggle = true;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void boutonEnvoyer_Click(object sender, RoutedEventArgs e)
+        {
+            if (toggle)
+            {
+                boutonEnvoyer.Background = Brushes.Beige;
+                toggle = !toggle;
+            }
+            else
+            {
+                boutonEnvoyer.Background = Brushes.RoyalBlue;
+                toggle = !toggle;
+            }
+        }   
     }
 }
